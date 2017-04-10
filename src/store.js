e1f82f14c2efb447cd4b9644a5385abc5af3fd43
zondezatera <zatera1920@gmail.com>
2017-04-10 18:29:09 +0700

    update ui

--- src/store.js
+++ src/store.js
@@ -1,15 +1,17 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-import { composeWithDevTools } from 'remote-redux-devtools';
+import devToolsEnhancer from 'remote-redux-devtools';
 
 export default (initialState) => {
   let middleware = applyMiddleware(thunk);
   if (process.env.NODE_ENV !== 'production') {
-    const composeEnhancers = composeWithDevTools({ realtime: true, port: 8000 });
-    middleware = composeEnhancers(middleware);
+    const devToolsExtension = window.devToolsExtension;
+    if (typeof devToolsExtension === 'function') {
+      middleware = compose(middleware, devToolsExtension());
+    }
   }
-  const store = createStore(reducers, initialState, middleware);
+  const store = createStore(reducers, initialState, middleware, devToolsEnhancer());
   if (module.hot) {
     module.hot.accept('./reducers', () => {
       store.replaceReducer(require('./reducers').default);
